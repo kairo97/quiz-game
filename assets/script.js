d1770490ca6602f6c9ac8764e9813c4a739ddbe3
Kairo97 <109006341+kairo97@users.noreply.github.com>
2022-12-27 14:13:45 -0800

    pseudocoded js file with details for how to move forawrd, filled out first 5 questions of quiz in html file

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// 1 TODO: make opening page that dexribes the quiz's content breifly
+// 2 TODO: make a button on opening page that will change page to start of game
+// 3 TODO: make a page with the first question and four possible answers, each clickable
+// 4 TODO: make each answer diplay opaque on hover and on click display correct or wrong at bottom of page
+// 5 TODO: make timer in corner of page recording how long each question takes
+// 6 TODO: make a button at bottom page, only displayed after answer is clicked to move to next question
+// 7 TODO: repeat steps 2-6 over 5-10 pages of questions
+// 8 TODO: log number of right and wrong questions
+// 9 TODO: log amount of time taken on each question
+// 10 TODO: determine score based on questions answered correctly
+// 11 TODO: diplay score at end of quiz
+// 12 TODO: give option to log score with name on highscore page
+
+
