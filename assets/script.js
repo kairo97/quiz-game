1be1498d1af40fb6830d3dee4d461bfed1edec2c
Kairo97 <109006341+kairo97@users.noreply.github.com>
2022-12-30 14:40:30 -0800

    reworked questions into js not html, reworked js to cycle

--- assets/script.js
+++ assets/script.js
@@ -10,151 +10,91 @@
 // 10 TODO: determine score based on questions answered correctly
 // 11 TODO: diplay score at end of quiz
 // 12 TODO: give option to log score with name on highscore page
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-btn')
 
-const questions = [{
-    id:0,
-    q: "What is the real name of Superman?",
-    a: [{text: "A. Clark Kent", isCorrect: false},
-        {text: "B. Kent Clark", isCorrect: false},
-        {text: "C. Calvin Ellis", isCorrect: false},
-        {text: "D. Kal-El", isCorrect: true}]
-},
-{
-    id:1,
-    q: "Who killed Bruce Wayne's parents?",
-    a:[{ text: "A. the Joker", isCorrect: false},
-       { text: "B. A random street thug", isCorrect: false},
-       { text: "C. Joe Chill", isCorrect: true},
-       { text: "D. Oswald Cobblepot", isCorrect: false}]
-},
-{
-    id:2,
-    q: "Who out of the following has not been a Robin?",
-    a:[{ text: "A. Dick Grayson", isCorrect: false},
-       { text: "B. Damien Wayne", isCorrect: false},
-       { text: "C. Jace Fox", isCorrect: true},
-       { text: "D. Jason Todd", isCorrect: false}]
-},
-{
-    id:3,
-    q: "Dr. Stephen Vincent Strange MD PHD is a doctor of what field of medicine?",
-    a:[{ text: "A. Neurosurgery", isCorrect: true},
-      { text: "B. Psychology", isCorrect: false},
-      { text: "C. Pediatrics", isCorrect: false},
-      { text: "D. Cancer Research", isCorrect: false}]
-},
-{
-    id:4,
-    q: "Which Marvel character was created specifically as a parody of a DC character?",
-    a:[{ text: "A. Nick Fury", isCorrect: false},
-       { text: "B. Deadpool", isCorrect: true},
-       { text: "C. Black Widow", isCorrect: false},
-       { text: "D. Moon Knight", isCorrect: false}]
-},
-{
-    id:5,
-    q: "In the Ultimate Spider-Man comic book series written by Brian Michael Bendis what is the origin of the Venom symbiote?",
-    a:[{ text: "A. Alien lifeform from an asteroid", isCorrect: false},
-       { text: "B. Made on purpose by Dr. Otto Octavius", isCorrect: false},
-       { text: "C. A failed cure for cancer", isCorrect: true},
-       { text: "D. It remains a mystery", isCorrect: false}]
-},
-{
-    id:6,
-    q: "The original Green lantern, Alan Scott has a weakness to what?",
-    a:[{ text: "A. The color yellow", isCorrect: false},
-       { text: "B. Magic", isCorrect: false},
-       { text: "C. Fear Itself", isCorrect: false},
-       { text: "D. Wood", isCorrect: true}]
-},
-{
-    id:7,
-    q: "the famous character from StarWars Darth Vader was inspired by what comic book character?",
-    a:[{ text: "A. Dr. Victor Von Doom", isCorrect: true},
-       { text: "B. Iron Man (Tony Stark)", isCorrect: false},
-       { text: "C. Black Mask (Roman Sionis)", isCorrect: false},
-       { text: "D. Bane (Antonio Diego)", isCorrect: false}]
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+function startGame() {
+    console.log("started")
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+
+}
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer) 
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+}
+}
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+var questions = [
+    {
+    question: 'What is the real name of superman?',
+    answers: [
+        { text: 'Clark Kent', correct: false },
+        { text: 'Kent Clark', correct: false },
+        { text: 'Calvin Ellis', correct: false},
+        { text: 'Kal-El', correct: true}
+    ]
 }
 ]
-var start = true;
-function iterate(id) {
-    var result = document.getElementsByClassName("result");
-    result[0].innerText = "";
-
-    const question = document.getElementById("question");
-    question.innerText = questions[id].q;
-
-    const op1 = document.getElementById('op1');
-    const op2 = document.getElementById('op2');
-    const op3 = document.getElementById('op3');
-    const op4 = document.getElementById('op4');
-
-    op1.innerText = questions[id].a[0].text;
-    op2.innerText = questions[id].a[1].text;
-    op3.innerText = questions[id].a[2].text;
-    op4.innerText = questions[id].a[3].text;
-
-    op1.value = questions[id].a[0].isCorrect;
-    op2.value = questions[id].a[1].isCorrect;
-    op3.value = questions[id].a[2].isCorrect;
-    op4.value = questions[id].a[3].isCorrect;
-
-    var selected = "";
-
-    op1.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue"; 
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor ="lightgoldenrodyellow" ;
-        selected = op1.value;
-    })
-
-    op2.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue"; 
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor ="lightgoldenrodyellow" ;
-        selected = op2.value;
-    })
-    op3.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue"; 
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor ="lightgoldenrodyellow" ;
-        selected = op3.value;
-    })
-    op4.addEventListener("click", () => {
-        op1.style.backgroundColor = "lightskyblue"; 
-        op2.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor = "lightskyblue";
-        op3.style.backgroundColor ="lightgoldenrodyellow" ;
-        selected = op4.value;
-    })
-    const evaluate = document.getElementsByClassName("evaluate");
-
-    evaluate[0].addEventListener("click", () => {
-        if (selected == "true") {
-            result[0].innerHTML = "true";
-            result[0].style.color = "green";
-        } else {
-            result[0].innerHTML = "false";
-            result[0].style.color = "red";
-        }
-    })
-}
-if (start) {
-    iterate("0");
-}
-
-const next = document.getElementsByClassName ('next')[0];
-var id = 0;
-
-next.addEventListener("click", () => {
-    start = false;
-    if (id < 2) {
-        id++;
-        iterate(id);
-        console.log(id);
-    }
-})
-
